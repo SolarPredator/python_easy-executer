3461586cd4f323f76fa3987007d76cb8f406a148
SolarPredator <balderfrandsen353@gmail.com>
2023-08-26 16:41:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,22 +10,100 @@ Select = ""
 custom_path = ""
 python_path = ""
 main_path = ""
+found ="hello"
 loc2 = ""  #short for location 2
 item_count = 0
-
+system = ""
 folder_number = 1
+clear_command = ""
+to_send = ""
 bat_files = []
 py_files = []
 execute_menu_items = []
+current_ = []
+current_list = []
+
+files_found_list = []
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+
+
+def has_batch(to_check):
+    global found, custom_path, current_list, to_send
+    last = os.getcwd()
+
+    current = to_check
+    current_ = os.scandir()
+    for item3 in current_:
+
+      if (item3.is_dir() and item3.name == to_check):
+        current_list = []
+        current_list.append(item3.name)
+
+    for item in current_list:
+        os.chdir(item)
+        current2 = os.scandir()
+
+        for item2 in current2:
+
+          if item2.name.endswith(".bat"):
+              found = (bcolors.OKGREEN + "BAT FILE FOUND" + bcolors.ENDC)
+              time.sleep(0.1)
+              to_send = os.path.join(to_check, found)
+              os.chdir(rf"{last}")
+              return to_send
+              break
+          else:
+              found = (bcolors.FAIL +"NO BAT FILE FOUND" + bcolors.ENDC)
+          to_send = os.path.join(to_check, found)
+          os.chdir(rf"{last}")
+    return to_send
+
+
+
+
+
+
+def check_platform():
+    global system
+    if sys.platform.startswith('linux'):
+        return "linux"
+    elif sys.platform.startswith('win'):
+        return "Windows"
+    else:
+        print("ERROR: Unkown system")
+        quit()
+
+
+
+if check_platform() == "linux":
+    clear_command = "clear"
+elif check_platform() == "Windows":
+    clear_command = "cls"
+else:
+    print("could not optimize the program for you're operation system")
+    quit()
 
 def config_file_setup():
-    global path, custom_path, python_path, main_path
+    global path, custom_path, python_path, main_path, system
     print("ERROR: CONFIG FILE NOT FOUND.")
     print("DO YOU WANT TO CREATE A CONFIG FILE?\nY/N")
     while True:
 
       if (keyboard.is_pressed("Y") or keyboard.is_pressed("y")):
-          subprocess.run("cls", shell=True)
+          subprocess.run(clear_command, shell=True)
           python_path = input("python.exe path: ")
           main_path = os.getcwd()
           path = input("\n\nuse custom(beta) or Default path?  C/D \n")
@@ -39,7 +117,7 @@ def config_file_setup():
           if os.path.exists("main.py") == True:
               print("creating file...")
               with open("config.txt", "w") as config_file:
-                  config_file.write(f'[setup]\npath = {path}\ncustom_path = {custom_path}\npython_path = {python_path}\nmain_path = {main_path}')
+                  config_file.write(f'[setup]\npath = {path}\ncustom_path = {custom_path}\npython_path = {python_path}\nmain_path = {main_path}\noperation_system = {check_platform()}')
               if os.path.exists("config.txt") == True:
                   print("Done!\nThe program needs to create a batch file for it to work. If not automaticly started in 5 seconds, restart the program")
                   time.sleep(5)
@@ -47,22 +125,23 @@ def config_file_setup():
 
 
 
+if os.path.exists("config.txt") == False:
+    config_file_setup()
 
-
-if os.path.exists("config.txt") == True:
+elif os.path.exists("config.txt") == True:
    config = configparser.ConfigParser()
    config.read_file(open(r'config.txt'))
    path = config.get('setup', 'path')
    python_path = config.get("setup", "python_path")
    main_path = config.get("setup", "main_path")
-elif os.path.exists("config.txt") == False:
-    config_file_setup()
+   system = config.get("setup", "operation_system")
+
 else:
     print("ERROR FINDING STATE OF config.txt")
     quit()
 
 
-if (os.path.exists("start.bat") == False or path == "default"): #if the program does not find the start.bat file, it will ask the user to create one
+if (os.path.exists("start.bat") == False or path == "default" and not os.path.exists("FILES")): #if the program does not find the start.bat file, it will ask the user to create one
 
     if os.path.exists("start.bat") == False:   #check if the start.bat file is present
       loc1 = python_path
@@ -131,8 +210,9 @@ def create_bat(file):
 
 
 def create_menu():
-    global files_found
-    subprocess.run('cls', shell=True)
+    global files_found, clear_command, item_count, Select, py_files
+    item = 0
+    subprocess.run(clear_command, shell=True)
     print("#" * 20)
     print("    EASY EXECUTE")
     print("#" * 20)
@@ -151,29 +231,31 @@ def create_menu():
         print("ERROR:7 ")
         quit()
     files_found = os.listdir(path=None)
+    py_files = []
     for item in files_found:
-        global py_files
+
         if os.path.isdir(item):
             py_files.append(item)
+            item_count = 0
     for item in py_files:
-        global Select, item_count
 
         print(f"{folder_number}.{py_files[item_count]}")
         folder_number = folder_number + 1
         item_count = item_count + 1
         time.sleep(0.1)
     while True:
-        time.sleep(1)
+        time.sleep(0.5)
         Select = keyboard.read_key()
         if Select != "":
             if Select.isdigit():
                 Select = int(Select)
-                files_found = os.listdir(path=None)
+                files_found = py_files
                 if 1 <= Select <= len(files_found):
                     selected_file = files_found[Select - 1]
                     print(f"You selected {selected_file}\n")
                     create_bat(selected_file)
-
+                elif Select == 0:
+                    main_menu()
 
 def open_file(file):
     global files_found, folder_number, custom_path, path
@@ -203,9 +285,9 @@ def open_file(file):
     os.startfile(bat_file)
     quit()
 def execute_menu():
-    global custom_path, path, execute_menu_items
+    global custom_path, path, execute_menu_items,clear_command
     # Create the CLI gui
-    subprocess.run('cls',shell=True)
+    subprocess.run(clear_command,shell=True)
     print("#" * 20)
     print("    EASY EXECUTE")
     print("#" * 20)
@@ -223,12 +305,12 @@ def execute_menu():
     elif path == "custom":
         os.chdir(rf"{custom_path}")
 
-    print(f"FILES FOUND IN(execute) {custom_path}:")
+    print(f"FILES FOUND IN(execute) {os.getcwd()}:")
     folder_number = 1
     for item in os.scandir():
         global Select
         if item.is_dir():
-          print(f"{folder_number}.{item.name}")
+          print(f"{folder_number}.{has_batch(item.name)}")
           folder_number = folder_number + 1
     while True:
       time.sleep(1)
@@ -236,9 +318,17 @@ def execute_menu():
       if Select != "":
         if Select.isdigit():
             Select = int(Select)
-            files_found = os.listdir(path=None)
-            if 1 <= Select <= len(files_found):
-                selected_file = files_found[Select - 1]
+            has_batch(custom_path)
+            files_found = os.scandir()
+
+            for files_found in files_found:
+
+                if files_found.is_dir():
+                    files_found_list.append(files_found.name)
+
+
+            if 1 <= Select <= len(files_found_list):
+                selected_file = files_found_list[Select - 1]
                 print(f"You selected {selected_file}\n")
                 open_file(selected_file)
 
@@ -256,8 +346,9 @@ def execute_menu():
 
 
 def main_menu():
+    global clear_command
     #Create the CLI gui
-    subprocess.run('cls', shell=True)
+    subprocess.run(clear_command, shell=True)
     print("#" * 20)
     print("    EASY EXECUTE")
     print("#" * 20)
@@ -273,5 +364,5 @@ def main_menu():
            create_menu()
 
 
-
+check_platform()
 main_menu()
